9e345806f19f47afad0a71abf489cc5c2b940088
PeeZapp <111848570+PeeZapp@users.noreply.github.com>
2022-08-23 14:41:46 +0800

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Jetpack":return img`
+f f f f f f f . . f f f f f f f 
+f 6 6 6 6 6 f . . f 6 6 6 6 6 f 
+f 6 6 6 6 6 f f f f 6 6 6 6 6 f 
+f 6 6 6 6 6 f f f f 6 6 6 6 6 f 
+f 6 6 6 6 6 f f f f 6 6 6 6 6 f 
+f 6 6 6 6 6 f f f f 6 6 6 6 6 f 
+f 6 6 6 6 6 f f f f 6 6 6 6 6 f 
+f 6 6 6 6 6 f f f f 6 6 6 6 6 f 
+f 6 6 6 6 6 f f f f 6 6 6 6 6 f 
+f 6 6 6 6 6 f . . f 6 6 6 6 6 f 
+f 6 6 6 6 6 f . . f 6 6 6 6 6 f 
+f f f f f f f . . f f f f f f f 
+4 9 9 9 4 4 . . . . 4 4 9 9 9 4 
+4 4 4 9 4 2 . . . . 2 4 9 4 4 4 
+2 4 4 4 2 . . . . . . 2 4 4 4 2 
+. 2 2 2 . . . . . . . . 2 2 2 . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
